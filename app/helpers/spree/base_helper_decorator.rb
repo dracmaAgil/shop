cbc625eaf3d73aa8a41f162b99704d112e88c32f
dracma_bucket <macrodracma@gmail.com>
2018-05-18 18:00:37 -0500

    cart views cleaned up

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,4 +1,9 @@
 module Spree::BaseHelper
+  
+  def layout_partial
+    devise_controller? ? 'spree/base/devise' : 'spree/base/application'
+  end
+
   def logo(image_path = Spree::Config[:logo], img_options: {})
     link_to image_tag(image_path, img_options), spree.root_path
   end
