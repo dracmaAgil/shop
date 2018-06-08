dca8c7a305788895ea0861edb00ce8a7bc9a239c
dracma_bucket <macrodracma@gmail.com>
2018-06-08 17:32:14 -0500

    order checkout

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,5 +1,16 @@
 module Spree::BaseHelper
-  
+
+  def flash_messages(opts = {})
+    ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+    flash.each do |msg_type, text|
+      unless ignore_types.include?(msg_type)
+        concat(content_tag(:div, text, class: "flash alert alert-#{msg_type}"))
+      end
+    end
+    nil
+  end
+
   def layout_partial
     devise_controller? ? 'spree/base/devise' : 'spree/base/application'
   end
---
+++ app/helpers/spree/checout_helper_decorator.rb
@@ -0,0 +1,26 @@
+module Spree::CheckoutHelper
+  def checkout_progress
+    states = checkout_states
+    items = states.map do |state|
+      text = link_to I18n.t("spree.order_state.#{state}").titleize, checkout_state_path(state), class: 'nav-link disabled'
+      css_classes = ['nav-item']
+      current_index = states.index(@order.state)
+      state_index = states.index(state)
+
+      if state_index < current_index
+        css_classes << 'completed'
+        text = link_to I18n.t("spree.order_state.#{state}").titleize, checkout_state_path(state), class: 'nav-link'
+      elsif state_index == current_index
+        text = link_to I18n.t("spree.order_state.#{state}").titleize, '#', class: 'nav-link active disabled'
+      end
+
+      css_classes << 'next' if state_index == current_index + 1
+      css_classes << 'active' if state == @order.state
+      css_classes << 'first' if state_index == 0
+      css_classes << 'last' if state_index == states.length - 1
+      # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
+      content_tag('li', text, class: css_classes)
+    end
+    content_tag('ol', raw(items.join("\n")), class: 'progress-steps nav nav-pills nav-fill', id: "checkout-step-#{@order.state}")
+  end
+end
